c18d8b7b01c3afe3749a4ab583186b966cedc6ae
Ratik Sharma <Ratik96@gmail.com>
2019-05-03 20:12:24 +0530

    Add Lesson 12 solutions

--- unit2/lesson12/q12.1.hs
+++ unit2/lesson12/q12.1.hs
@@ -1,1 +1,29 @@
 -- Write a function similar to canDonateTo that takes two patients as arguments rather than two BloodTypes.
+
+type FirstName = String
+type MiddleName = String
+type LastName = String
+
+data Name = Name FirstName LastName 
+            | NameWithMiddle FirstName MiddleName LastName
+data Sex = Male | Female
+data RhType = Pos | Neg
+data ABOType = A | B | AB | O
+data BloodType = BloodType ABOType RhType
+
+data Patient = Patient { name :: Name
+                       , sex :: Sex
+                       , age :: Int
+                       , height :: Int
+                       , weight :: Int
+                       , bloodType :: BloodType }
+
+canDonateTo :: BloodType -> BloodType -> Bool
+canDonateTo (BloodType O _) _ = True                     
+canDonateTo _ (BloodType AB _) = True
+canDonateTo (BloodType A _) (BloodType A _) = True
+canDonateTo (BloodType B _) (BloodType B _) = True
+canDonateTo _ _ = False -- otherwise
+
+donateFor :: Patient -> Patient -> Bool
+donateFor p1 p2 = canDonateTo (bloodType p1) (bloodType p2)
--- unit2/lesson12/q12.2.hs
+++ unit2/lesson12/q12.2.hs
@@ -1,7 +1,7 @@
 {-
 
 Implement a patientSummary function that uses your final Patient type.
-patient-Summary should output a string that looks like this: 
+patientSummary should output a string that looks like this: 
 
 **************
 Patient Name: Smith, John
@@ -16,4 +16,45 @@ If you need to, feel free to create useful helper functions.
 
 -}
 
+type FirstName = String
+type MiddleName = String
+type LastName = String
 
+data Name = Name FirstName LastName 
+            | NameWithMiddle FirstName MiddleName LastName
+data Sex = Male | Female
+data RhType = Pos | Neg
+data ABOType = A | B | AB | O
+data BloodType = BloodType ABOType RhType
+
+showRh :: RhType -> String
+showRh Pos = "+"
+showRh Neg = "-"
+
+showABO :: ABOType -> String
+showABO A = "A"
+showABO B = "B"
+showABO AB = "AB"
+showABO O = "O"
+
+showBloodType :: BloodType -> String
+showBloodType (BloodType abo rh)  = showABO abo ++ showRh rh
+
+data Patient = Patient { name :: Name
+                       , sex :: Sex
+                       , age :: Int
+                       , height :: Int
+                       , weight :: Int
+                       , bloodType :: BloodType }
+
+showSex Male = "Male"
+showSex Female = "Female"
+
+patientSummary :: Patient -> String
+patientSummary patient = "**************\n" ++
+                        "Sex: " ++ showSex (sex patient) ++ "\n" ++
+                        "Age: " ++ show (age patient) ++ "\n" ++
+                        "Height: " ++ show (height patient) ++ " in.\n" ++
+                        "Weight: " ++ show (weight patient) ++ " lbs.\n" ++
+                        "Blood Type: " ++ showBloodType (bloodType patient) ++
+                        "\n**************\n"
